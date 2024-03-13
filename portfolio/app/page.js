b27099fbb9a5ddc3925676413cfa957f5b6d0ee9
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 13:44:02 +0100

    feat: update page.js

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../components/header.js";
 import NavigationBar from "../components/navbar.js";
+import About from "../components/about.js";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-900">
     <NavigationBar/>
     <Header/>
+    <About />
     </main>
   );
 }
--- portfolio/components/navbar.js
+++ portfolio/components/navbar.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
 const NavigationBar = () => {
+
   return (
-    <nav class="text-white font-bold flex items-center">
-      <p class="text-white font-bold mr-4 text-l">
+    <nav class="text-white font-bold flex items-center fixed top-0 w-full bg-gray-900">
+      <p class="text-white font-bold mr-4">
         Alassane WADE
       </p>
-      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+      <button class="text-white font-bold py-2 px-4 transition-transform hover:scale-105">
         Home
       </button>
-      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+      <button class="text-white font-bold py-2 px-4 transition-transform hover:scale-105">
         About
       </button>
-      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+      <button class="text-white font-bold py-2 px-4 transition-transform hover:scale-105">
         Experiences
       </button>
-      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+      <button class="text-white font-bold py-2 px-4 transition-transform hover:scale-105">
         Projects
       </button>
     </nav>
   );
+  
 };
 
 export default NavigationBar;
