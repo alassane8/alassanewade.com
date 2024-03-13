b1d7ab0cce12233a376e871f82237b7bda4d4eb7
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 13:27:34 +0100

    feat: added buttons on navbar

--- portfolio/components/navbar.js
+++ portfolio/components/navbar.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 const NavigationBar = () => {
   return (
-    <nav>
-    <p className="text-2xl font-bold text-white">
-      navbar
-    </p>
+    <nav class="text-white font-bold flex items-center">
+      <p class="text-white font-bold mr-4 text-l">
+        Alassane WADE
+      </p>
+      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+        Home
+      </button>
+      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+        About
+      </button>
+      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+        Experiences
+      </button>
+      <button class="text-white font-bold py-2 px-2 transition-transform hover:scale-110 hover:text-blue-500">
+        Projects
+      </button>
     </nav>
   );
 };
