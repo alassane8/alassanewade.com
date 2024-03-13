46e25d764345bf847a5772f2d4333fa2f84114e7
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 11:13:30 +0100

    feat: change main className

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -4,7 +4,7 @@ import NavigationBar from "../components/navbar.js";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-900">
+    <main className="min-h-screen bg-gray-900">
     <NavigationBar/>
     <Header/>
     </main>
