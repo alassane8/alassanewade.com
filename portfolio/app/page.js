b7422637fa311aa92599bbedd3c63401006772e3
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 15:23:28 +0100

    feat: add lasts components

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Header from "../components/header.js";
 import NavigationBar from "../components/navbar.js";
 import About from "../components/about.js";
+import Experiences from "../components/experiences.js";
+import Projects from "../components/projects.js";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-900">
-    <NavigationBar/>
-    <Header/>
-    <About />
+      <NavigationBar/>
+      <Header/>
+      <About />
+      <Experiences />
+      <Projects />
     </main>
   );
 }
