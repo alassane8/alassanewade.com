372d9ae9dc1a35208ee9f89caf1b361b856f4902
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 19:41:34 +0100

    feat: page.js

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-900">
       <NavigationBar/>
-      <Header/><br /><br /><br /><br />
-      <About /><br /><br /><br /><br />
-      <Experiences /><br /><br /><br /><br />
+      <Header/>
+      <About />
+      <Experiences />
       <Projects />
     </main>
   );
