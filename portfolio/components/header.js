f0e30e069591d2af09a126922e35a39f50519579
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 17:43:34 +0100

    feat: added contact me button

--- portfolio/components/header.js
+++ portfolio/components/header.js
@@ -14,20 +14,29 @@ const Header = () => {
           </h1>
           <div className="grid place-items-center">
             <p className="text-5xl font-bold text-white">
-                I enjoy doing personal projects.<br /><br />
+              I like to code and create things.<br /><br />
             </p>
-            <p className="text-2xl font-bold text-white">
+            <p className="text-xl font-bold text-white">
                 I am a {' '}
                 <span className="text-blue-500">
                   Software Engineer
                 </span>{' '}
-                majoring <br />in {' '}
+                from {' '}
                 <span className="text-blue-500">
-                  Cybersecurity
+                  Paris
+                </span>
+                , in France<br />
+                and I enjoy very much programming languages as it offers <br />{' '}
+                <span className="text-blue-500">
+                  countless possiblities
                 </span>{' '}
-                in Paris, France.
+                  to create what one has in mind.<br /><br /><br />
             </p>
           </div>
+          <button class="border-white border-t-4 border-l-4 border-r-4 border-b-4 bg-gray-900 text-2xl text-white font-bold px-7 py-7 rounded-md hover:scale-110 hover:text-blue-300 ">
+          <a href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRpccgnBHnpWBsdDfwHSHRzHXmJPKrJTFcDgdpKTLwMKLzBfGDJrtXszGHmhcktHdsbSwNV">Contact me!</a>
+          </button>
+
         </div>
       </div>
     </header>
