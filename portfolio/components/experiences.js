59b074af1978285c118450136071c9a06728b0a7
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 19:37:16 +0100

    feat: experiences.js in progress

--- portfolio/components/experiences.js
+++ portfolio/components/experiences.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Experiences = () => {
+
   return (
-    <div id="experiences">
-      
+    <div id="experiences" className = "ml-20 min-h-screen bg-gray-900">
+      <h1 className = "text-6xl font-bold text-white"> 
+      <br />/experiences/<br /><br />
+      </h1>
     </div>
   );
 };
