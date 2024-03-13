e02ac4df5d86633423202d42f5c86e9f7e08e8d8
alassane8 <alassane.wade@edu.ece.fr>
2024-03-13 10:32:49 +0100

    feat: update style

--- portfolio/components/header.js
+++ portfolio/components/header.js
@@ -3,19 +3,21 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <div className="grid place-items-center h-screen">
-        <h1 className="text-8xl font-bold text-white justify-center">
+      <div className="grid place-items-center">
+        <h1 className="text-8xl font-bold text-white">
           hi, it's{' '}
           <span className="text-blue-400 animate-fade-in">
-            Alassane.
+            Alassane.<br /><br />
           </span>{' '}
         </h1>
-        <p className="text-5xl font-bold text-white grid place-items-center">
+        <div className="grid place-items-center">
+        <p className="text-5xl font-bold text-white">
             I enjoy doing personal projects!<br /><br />
+        </p>
         <p className="text-2xl font-bold text-white">
             I am a Software Engineer majoring<br />in Cybersecurity in Paris, France.
         </p>
-        </p>
+        </div>
       </div>
     </header>
   );
