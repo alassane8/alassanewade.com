46f231f429201b214d4b2064251003316ffd74a3
alassane8 <alassane.wade@edu.ece.fr>
2024-03-14 15:57:52 +0100

    feat: update

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from "../components/header.js";
-import NavigationBar from "../components/navbar.js";
 import About from "../components/about.js";
-import Experiences from "../components/experiences.js";
-import Projects from "../components/projects.js";
+import Header from "../components/header.js";
 import Footer from "../components/footer.js";
+import Projects from "../components/projects.js";
+import NavigationBar from "../components/navbar.js";
+import Experiences from "../components/experiences.js";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-900">
-      <NavigationBar/>
+    <NavigationBar/>
       <Header/>
       <About />
       <Experiences />
