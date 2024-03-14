87df4d1093eda0c2220b39be5b0840591c126260
alassane8 <alassane.wade@edu.ece.fr>
2024-03-14 10:00:31 +0100

    chore: create footer.js

---
+++ portfolio/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className = "mx-auto bg-gray-900 max-w-6xl">
+        <h1 className = "text-white font bold" align= "center">
+            Copyrights &#xa9; 2023 Alassane WADE. All rights reserved.<br /><br />
+        </h1>
+    </div>
+  );
+};
+export default Footer;
