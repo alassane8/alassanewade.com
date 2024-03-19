e5a6a56c17b6582c6c1d46c08ff98d4245d04a6d
alassane8 <alassane.wade@edu.ece.fr>
2024-03-19 14:25:34 +0100

    feat: update projects.js

--- portfolio/app/components/experiences.js
+++ portfolio/app/components/experiences.js
@@ -7,6 +7,7 @@ const Experiences = () => {
       <h1 className = "text-6xl font-bold text-white"> 
       <br />/experiences/<br /><br />
       </h1>
+      
     <div className = "h-screen mx-auto bg-gray-800 max-w-6xl">
       
     </div>
