facf68a1546fea9068770ef82f9ca74b8d290532
alassane8 <alassane.wade@edu.ece.fr>
2024-03-19 17:06:13 +0100

    feat: updated navbar

--- portfolio/app/components/navbar.js
+++ portfolio/app/components/navbar.js
@@ -2,34 +2,36 @@ import React from 'react';
 
 const NavigationBar = () => {
   return (
-    <nav className="text-white font-bold flex items-center justify-between w-full fixed bg-gray-800">
-      <div className="flex items-center">
-        <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
-          <a href="#home">home</a>
-        </button>
-        <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
-          <a href="#about">about</a>
-        </button>
-        <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
-          <a href="#experiences">experiences</a>
-        </button>
-        <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
-          <a href="#projects">projects</a>
-        </button>
+    <div id="about" className = "mx-auto bg-gray-800 max-w-6xl">
+      <div className="text-white font-bold flex items-center fixed bg-gray-800">
+        <div className="flex items-center">
+          <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
+            <a href="#home">home</a>
+          </button>
+          <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
+            <a href="#about">about</a>
+          </button>
+          <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
+            <a href="#experiences">experiences</a>
+          </button>
+          <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
+            <a href="#projects">projects</a>
+          </button>
+        </div>
+        <div className="flex items-center">
+          <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-110 hover:text-blue-300 mx-2">
+            <a href="https://github.com/alassane8">
+              <img src="images/github.png" alt="Github" class="h-8 w-8"></img>
+            </a>
+          </button>
+          <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-110 hover:text-blue-300 mx-2">
+            <a href="https://www.linkedin.com/in/alassane-wade-91181725b/">
+              <img src="images/linkedin.png" alt="LinkedIn" class="h-8 w-8"></img>
+            </a>
+          </button>        
+        </div>
       </div>
-      <div className="flex items-center">
-        <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-110 hover:text-blue-300 mx-2">
-          <a href="https://github.com/alassane8">
-            <img src="images/github.png" alt="Github" class="h-8 w-8"></img>
-          </a>
-        </button>
-        <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-110 hover:text-blue-300 mx-2">
-          <a href="https://www.linkedin.com/in/alassane-wade-91181725b/">
-            <img src="images/linkedin.png" alt="LinkedIn" class="h-8 w-8"></img>
-          </a>
-        </button>        
-      </div>
-    </nav>
+    </div>
   );
 };
 
