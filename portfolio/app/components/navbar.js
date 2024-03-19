a65f2547135c1e49adc384b72b5ad3ed90a832e1
alassane8 <alassane.wade@edu.ece.fr>
2024-03-19 22:17:04 +0100

    feat: navbar.js completed

--- portfolio/app/components/navbar.js
+++ portfolio/app/components/navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NavigationBar = () => {
   return (
-    <div id="about" className = "mx-auto bg-gray-800 max-w-6xl">
-      <div className="text-white font-bold flex items-center fixed bg-gray-800">
-        <div className="flex items-center">
+    <div id="about" className="mx-auto bg-gray-800 max-w-6xl">
+      <div className="text-white font-bold flex items-center bg-gray-800">
+        <div className="flex items-center w-full">
           <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-105 hover:text-blue-300 mx-2">
             <a href="#home">home</a>
           </button>
@@ -21,14 +21,14 @@ const NavigationBar = () => {
         <div className="flex items-center">
           <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-110 hover:text-blue-300 mx-2">
             <a href="https://github.com/alassane8">
-              <img src="images/github.png" alt="Github" class="h-8 w-8"></img>
+              <img src="images/github.png" alt="Github" className="h-8 w-11"></img>
             </a>
           </button>
           <button className="text-white font-bold py-2 px-4 transition-transform hover:scale-110 hover:text-blue-300 mx-2">
             <a href="https://www.linkedin.com/in/alassane-wade-91181725b/">
-              <img src="images/linkedin.png" alt="LinkedIn" class="h-8 w-8"></img>
+              <img src="images/linkedin.png" alt="LinkedIn" className="h-8 w-11"></img>
             </a>
-          </button>        
+          </button>
         </div>
       </div>
     </div>
