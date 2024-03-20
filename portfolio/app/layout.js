0f86b8175802d2d1fb699ef5436a6b26aa39cb2c
alassane8 <alassane.wade@edu.ece.fr>
2024-03-20 12:59:21 +0100

    feat: change title page

--- portfolio/app/layout.js
+++ portfolio/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Alassane",
+  title: "alassane",
   description: "Portfolio displaying my professionnal experiences and projects",
 };
 
